525870fee1a15c847770f70fd55361b9b6c41c8e
sheroanh <anhnguyentuan8@gmail.com>
2023-05-12 10:34:18 +0700

    update dot-env

--- src/main/java/rhm/database/Oracle.java
+++ src/main/java/rhm/database/Oracle.java
@@ -2,11 +2,13 @@ package rhm.database;
 
 import java.sql.*;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class                                                                                                                                   Oracle {
-    // private final String DB_URL = "jdbc:oracle:thin:@localhost:1521/orcl";
-    private final String DB_URL = "jdbc:oracle:thin:@localhost:1521/xe";
-    private final String DB_USERNAME = "SHEROANH";
-    private final String DB_PASSWORD = "Tuananh2003";
+    Dotenv dotenv = Dotenv.load();
+    private final String DB_URL = dotenv.get("DB_URL");
+    private final String DB_USERNAME = dotenv.get("DB_USERNAME");
+    private final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
 
     public Connection getConnection() {
         Connection conn = null;
