7f97e608212309f83a13ad913bc6205725d3042d
Whitie017 <nguyenngoc091203@gmail.com>
2023-05-23 13:09:05 +0700

    test

--- src/main/java/com/theuntidycat/rhm/App.java
+++ src/main/java/com/theuntidycat/rhm/App.java
@@ -2,12 +2,13 @@ package com.theuntidycat.rhm;
 
 import com.theuntidycat.rhm.view.LoginView;
 import com.theuntidycat.rhm.view.ManageContract;
+import com.theuntidycat.rhm.view.ContractFrameMain;
 
 public class App{
     public static void main(String[] args)
     {
 //        LoginView loginWindow = new LoginView();
 //        loginWindow.run();
-          ManageContract test = new ManageContract();
+          ContractFrameMain test = new ContractFrameMain();
     }    
 }
