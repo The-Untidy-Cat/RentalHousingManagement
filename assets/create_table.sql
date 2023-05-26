f6dd848d5bd4fae9ce88522bfe2a366ccfd7b398
sheroanh <anhnguyentuan8@gmail.com>
2023-05-26 08:34:47 +0700

    update sql code

--- assets/create_table.sql
+++ assets/create_table.sql
@@ -54,7 +54,7 @@ CREATE TABLE tenant (
     phone_number VARCHAR2(10) UNIQUE,
     password     VARCHAR2(255),
     email        VARCHAR2(255) UNIQUE,
-    dob date not null,
+    dob          DATE NOT NULL,
     id_number    VARCHAR2(12),
     status_id    NUMBER(1, 0) DEFAULT 1,
     CONSTRAINT tenant_status_id_fk FOREIGN KEY ( status_id )
@@ -82,114 +82,8 @@ CREATE OR REPLACE TRIGGER create_password BEFORE
     INSERT ON tenant
     FOR EACH ROW
 BEGIN
-    :new.password := concat(concat(TO_CHAR(:NEW.DOB, 'DD'),TO_CHAR(:NEW.DOB, 'MM')),TO_CHAR(:NEW.DOB, 'YYYY'));
+    :new.password := concat(concat(to_char(:new.dob, 'DD'), to_char(:new.dob, 'MM')), to_char(:new.dob, 'YYYY'));
 END;
-/
-
-CREATE TABLE support_ticket_type (
-    id   NUMBER(1, 0) PRIMARY KEY,
-    name VARCHAR2(255)
-);
-/
-
-CREATE TABLE support_ticket_status (
-    id   NUMBER(1, 0) PRIMARY KEY,
-    name VARCHAR2(255)
-);
-/
----------------SUPPORT TICKET---------------
-CREATE TABLE support_ticket (
-    id            NUMBER PRIMARY KEY,
-    type_id       NUMBER(1, 0) DEFAULT 0 NOT NULL,
-    incident_time DATE,
-    receive_time  DATE,
-    room_id       VARCHAR2(5) NOT NULL,
-    tenant_id     VARCHAR2(5) NOT NULL,
-    description   VARCHAR2(50),
-    status_id     NUMBER(1, 0) DEFAULT 0,
-    CONSTRAINT support_ticket_room_id_fk FOREIGN KEY ( room_id )
-        REFERENCES room ( id ),
-    CONSTRAINT support_ticket_tenant_id_fk FOREIGN KEY ( tenant_id )
-        REFERENCES tenant ( id ),
-    CONSTRAINT support_ticket_type_id_fk FOREIGN KEY ( type_id )
-        REFERENCES support_ticket_type ( id ),
-    CONSTRAINT support_ticket_status_id_fk FOREIGN KEY ( status_id )
-        REFERENCES support_ticket_status ( id )
-);
-/
-
-CREATE OR REPLACE TRIGGER ticketid_increment BEFORE
-    INSERT ON support_ticket
-    FOR EACH ROW
-BEGIN
-    SELECT
-        concat('HT',
-               lpad(COUNT(*) + 1,
-                    3,
-                    0))
-    INTO :new.id
-    FROM
-        support_ticket;
-
-END;
-/
-
-CREATE TABLE invoice_status (
-    id   NUMBER(1, 0) PRIMARY KEY,
-    name VARCHAR2(255)
-);
-/
- ---------------INVOICE---------------
-CREATE TABLE invoice (
-    id          VARCHAR2(5) PRIMARY KEY,
-    room_id     VARCHAR2(5),
-    month       NUMBER,
-    year        NUMBER,
-    total_money NUMBER,
-    status_id   NUMBER(1, 0) DEFAULT 0,
-    CONSTRAINT invoice_room_id_fk FOREIGN KEY ( room_id )
-        REFERENCES room ( id ),
-    CONSTRAINT invoice_status_id_fk FOREIGN KEY ( status_id )
-        REFERENCES invoice_status ( id )
-);
-/
-
-CREATE OR REPLACE TRIGGER invoiceid_increment BEFORE
-    INSERT ON invoice
-    FOR EACH ROW
-BEGIN
-    SELECT
-        concat('HN',
-               lpad(COUNT(*) + 1,
-                    3,
-                    0))
-    INTO :new.id
-    FROM
-        invoice;
-
-END;
-/
-
-CREATE TABLE detail_invoice_type (
-    id   NUMBER(1, 0) PRIMARY KEY,
-    name VARCHAR2(255),
-    unit VARCHAR2(255)
-);
-/
- ---------------DETAIL_INVOICE---------------
-CREATE TABLE detail_invoice (
-    invoice_id VARCHAR2(5),
-    type_id    NUMBER(1, 0),
-    quantity   NUMBER(3, 0),
-    unit_price NUMBER,
-    sum_money  NUMBER,
-    CONSTRAINT detail_invoice_pk PRIMARY KEY ( invoice_id,
-                                               type_id ),
-    CONSTRAINT detail_invoice_invoice_id_fk FOREIGN KEY ( invoice_id )
-        REFERENCES invoice ( id ),
-    CONSTRAINT detail_invoice_type_id_fk FOREIGN KEY ( type_id )
-        REFERENCES detail_invoice_type ( id )
-);
 /
 
 CREATE TABLE contract_status (
@@ -249,28 +143,71 @@ CREATE TABLE detail_contract (
                                                 tenant_id )
 );
 /
- ---------------TRIGGER DELETE INVOICE---------------
-CREATE OR REPLACE TRIGGER delete_detail_invoice BEFORE
-    DELETE ON detail_invoice
+
+CREATE OR REPLACE TRIGGER set_price_per_period BEFORE
+    INSERT ON contract
     FOR EACH ROW
 DECLARE
-    temp_invoice_status_id invoice.status_id%TYPE;
+    temp_price room.rental_price%TYPE;
 BEGIN
     SELECT
-        status_id
-    INTO temp_invoice_status_id
+        rental_price
+    INTO temp_price
     FROM
-        invoice
+        room
     WHERE
-        id = :old.invoice_id;
+        id = :new.room_id;
 
-    IF temp_invoice_status_id = 1 THEN
-        raise_application_error(-20111, 'Can not delete invoice which is paid');
-    END IF;
+    :new.price_per_period := temp_price;
 END;
 /
+-- Gom 2 cai thanh 1 trigger va phan thanh trigger dong goi thanh 1 procedure
+CREATE OR REPLACE TRIGGER insert_representative AFTER
+    INSERT ON contract
+    FOR EACH ROW
+BEGIN
+    INSERT INTO detail_contract (
+        contract_id,
+        tenant_id
+    ) VALUES (
+        :new.id,
+        :new.tenant_id
+    );
+END;
+/
+CREATE OR REPLACE TRIGGER insert_roomates AFTER
+    INSERT ON detail_contract
+    FOR EACH ROW
+DECLARE
+    temp_max_tenant   room.capacity%type;
+    temp_count_tenant number;
+BEGIN
+    SELECT
+        capacity
+    INTO temp_max_tenant
+    FROM
+        contract join room on contract.room_id = room.id
+    WHERE
+        contract.id = :new.contract_id;
+
+    SELECT
+        COUNT(*)
+    INTO temp_count_tenant
+    FROM
+        detail_contract
+    WHERE
+        contract_id = :new.contract_id;
+
+    IF ( ( temp_count_tenant + 1 ) = temp_max_tenant ) THEN
+        raise_application_error(-20111, 'Can not insert tenant');
+    END IF;
+
+END;
+/
+
+
  ---------------TRIGGER INSERT REPRESENTATIVE WHO IS REPRESENTATIVE IN CONTRACT---------------
-CREATE OR REPLACE TRIGGER insert_representative BEFORE
+CREATE OR REPLACE TRIGGER check_status_of_contract AFTER
     INSERT ON detail_contract
     FOR EACH ROW
 DECLARE
@@ -290,45 +227,103 @@ BEGIN
 END;
 /
  ---------------TRIGGER INSERT TENANT IN DETAIL_CONTRACT---------------
-CREATE OR REPLACE TRIGGER insert_roomates BEFORE
-    INSERT ON detail_contract
+
+CREATE TABLE support_ticket_status (
+    id   NUMBER(1, 0) PRIMARY KEY,
+    name VARCHAR2(255)
+);
+/
+
+---------------SUPPORT TICKET---------------
+CREATE TABLE support_ticket (
+    id            VARCHAR2(5) PRIMARY KEY,
+    incident_time DATE,
+    receive_time  DATE,
+    room_id       VARCHAR2(5) NOT NULL,
+    tenant_id     VARCHAR2(5) NOT NULL,
+    description   VARCHAR2(255),
+    status_id     NUMBER(1, 0) DEFAULT 0,
+    CONSTRAINT support_ticket_room_id_fk FOREIGN KEY ( room_id )
+        REFERENCES room ( id ),
+    CONSTRAINT support_ticket_tenant_id_fk FOREIGN KEY ( tenant_id )
+        REFERENCES tenant ( id ),
+    CONSTRAINT support_ticket_status_id_fk FOREIGN KEY ( status_id )
+        REFERENCES support_ticket_status ( id )
+);
+/
+
+CREATE OR REPLACE TRIGGER ticketid_increment BEFORE
+    INSERT ON support_ticket
     FOR EACH ROW
-DECLARE
-    temp_max_tenant   NUMBER;
-    temp_count_tenant NUMBER;
-    temp_room_id      room.id%TYPE;
 BEGIN
     SELECT
-        room_id
-    INTO temp_room_id
+        concat('HT',
+               lpad(COUNT(*) + 1,
+                    3,
+                    0))
+    INTO :new.id
     FROM
-        contract
-    WHERE
-        id = :new.contract_id;
-
-    SELECT
-        capacity
-    INTO temp_max_tenant
-    FROM
-        room
-    WHERE
-        id = temp_room_id;
-
-    SELECT
-        COUNT(*)
-    INTO temp_count_tenant
-    FROM
-        detail_contract
-    WHERE
-        contract_id = :new.contract_id;
-
-    IF ( ( temp_count_tenant + 1 ) = temp_max_tenant ) THEN
-        raise_application_error(-20111, 'Room '
-                                        || temp_room_id
-                                        || ' is full. Can not insert tenant');
-    END IF;
+        support_ticket;
 
 END;
+/
+
+CREATE TABLE invoice_status (
+    id   NUMBER(1, 0) PRIMARY KEY,
+    name VARCHAR2(255)
+);
+/
+ ---------------INVOICE---------------
+CREATE TABLE invoice (
+    id          VARCHAR2(5) PRIMARY KEY,
+    contract_id VARCHAR2(5),
+    month       NUMBER,
+    year        NUMBER,
+    total_money NUMBER,
+    status_id   NUMBER(1, 0) DEFAULT 0,
+    CONSTRAINT invoice_contract_id_fk FOREIGN KEY ( contract_id )
+        REFERENCES contract ( id ),
+    CONSTRAINT invoice_status_id_fk FOREIGN KEY ( status_id )
+        REFERENCES invoice_status ( id )
+);
+/
+
+CREATE OR REPLACE TRIGGER invoiceid_increment BEFORE
+    INSERT ON invoice
+    FOR EACH ROW
+BEGIN
+    SELECT
+        concat('HN',
+               lpad(COUNT(*) + 1,
+                    3,
+                    0))
+    INTO :new.id
+    FROM
+        invoice;
+
+END;
+/
+
+CREATE TABLE detail_invoice_type (
+    id   NUMBER(1, 0) PRIMARY KEY,
+    name VARCHAR2(255),
+    unit VARCHAR2(255)
+);
+/
+ ---------------DETAIL_INVOICE---------------
+CREATE TABLE detail_invoice (
+    invoice_id VARCHAR2(5),
+    type_id    NUMBER(1, 0),
+    quantity   NUMBER(3, 0) DEFAULT NULL,
+    unit_price NUMBER DEFAULT NULL,
+    sum_money  NUMBER NOT NULL,
+    CONSTRAINT detail_invoice_pk PRIMARY KEY ( invoice_id,
+                                               type_id ),
+    CONSTRAINT detail_invoice_invoice_id_fk FOREIGN KEY ( invoice_id )
+        REFERENCES invoice ( id ),
+    CONSTRAINT detail_invoice_type_id_fk FOREIGN KEY ( type_id )
+        REFERENCES detail_invoice_type ( id )
+);
 /
 
  ---------------TRIGGER UPDATE STATUS WHEN ADD NEW CHILD INVOICE---------------
@@ -356,99 +351,55 @@ BEGIN
 END;
 /
 
+CREATE OR REPLACE TRIGGER delete_detail_invoice BEFORE
+    DELETE ON detail_invoice
+    FOR EACH ROW
+DECLARE
+    temp_invoice_status_id invoice.status_id%TYPE;
+BEGIN
+    SELECT
+        status_id
+    INTO temp_invoice_status_id
+    FROM
+        invoice
+    WHERE
+        id = :old.invoice_id;
+
+    IF temp_invoice_status_id = 1 THEN
+        raise_application_error(-20111, 'Can not delete invoice which is paid');
+    END IF;
+END;
+/
+
+--CREATE OR REPLACE TRIGGER new_rent_money_detail_invoice AFTER
+--    INSERT ON invoice
+--    FOR EACH ROW
+--DECLARE
+--    temp_sum_money detail_invoice.sum_money%TYPE;
+--BEGIN
+--    SELECT
+--        price_per_period
+--    INTO temp_sum_money
+--    FROM
+--        contract
+--    WHERE
+--        id = :new.contract_id;
+--
+--    INSERT INTO detail_invoice (
+--        invoice_id,
+--        type_id,
+--        sum_money
+--    ) VALUES (
+--        :new.id,
+--        0,
+--        temp_sum_money
+--    );
+--
+--END;
+--/
+
 CREATE TABLE account (
     username VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255)
 );
 /
- ---------------DEMO---------------
---INSERT INTO room ( roomname ) VALUES ( 'Room 1' );
---
---INSERT INTO invoice (
---    roomid,
---    totalmoney
---) VALUES (
---    'P001',
---    10000
---);
---
---UPDATE invoice
---SET
---    status = 1
---WHERE
---    invoiceid = 'HD001';
---
---DELETE FROM invoice
---WHERE
---    invoiceid = 'HD001';
---
---DELETE FROM room;
---
---DELETE FROM tenant;
---
---DELETE FROM contract;
---
---DELETE FROM detail_contract;
---
---INSERT INTO room (
---    roomname,
---    capacity
---) VALUES (
---    'Phong 1',
---    2
---);
---
---INSERT INTO tenant ( tenantname ) VALUES ( 'Khach A' );
---
---INSERT INTO tenant ( tenantname ) VALUES ( 'Khach B' );
---
---INSERT INTO tenant ( tenantname ) VALUES ( 'Khach C' );
---
---INSERT INTO tenant ( tenantname ) VALUES ( 'Khach D' );
---
---INSERT INTO contract (
---    contractid,
---    roomid,
---    tenantid
---) VALUES (
---    'HG001',
---    'P0001',
---    'KH001'
---);
---
---INSERT INTO detail_contract (
---    contractid,
---    roomid,
---    tenantid
---) VALUES (
---    'HG001',
---    'P0001',
---    'KH002'
---);
---
---INSERT INTO detail_contract (
---    contractid,
---    roomid,
---    tenantid
---) VALUES (
---    'HG001',
---    'P0001',
---    'KH003'
---);
---
---SELECT
---    *
---FROM
---    detail_contract
---WHERE
---    roomid = 'P0001';
---
---INSERT INTO detail_contract (
---    contractid,
---    roomid,
---    tenantid
---) VALUES (
---    'HG001',
---    'P0001',
---    'KH004'
---);
