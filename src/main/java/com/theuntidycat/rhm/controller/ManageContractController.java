1f2a1d05a0f82a7fbcada16e05f67234a3acfb9a
Whitie017 <nguyenngoc091203@gmail.com>
2023-06-01 08:44:29 +0700

    update contract

--- src/main/java/com/theuntidycat/rhm/controller/ManageContractController.java
+++ src/main/java/com/theuntidycat/rhm/controller/ManageContractController.java
@@ -17,7 +17,6 @@ public class ManageContractController
     private final Oracle database = new Oracle();
     private final Connection con = database.getConnection();
     
-    
     public ResultSet getCBBRoom()
     {
         ResultSet rs = null;
@@ -32,5 +31,26 @@ public class ManageContractController
             System.out.println("Error in Contract_GetCBBRoom");
         }
         return rs;
+    }
+    
+    public boolean insertContract(String start_date, String end_date, float price, float deposit, String tenant_id, String room_id)
+    {
+        try
+        {
+            PreparedStatement prestmt = con.prepareStatement("INSERT INTO CONTRACT(START_DATE, END_DATE, PRICE_PER_PERIOD, DEPOSIT, TENANT_ID, ROOM_ID, STATUS_ID) VALUES (?,?,?,?,?,?,1)");
+            prestmt.setString(1, start_date);
+            prestmt.setString(2, end_date);
+            prestmt.setFloat(3, price);
+            prestmt.setFloat(4, deposit);
+            prestmt.setString(5, tenant_id);
+            prestmt.setString(6, room_id);
+            
+            return true;
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e);
+            return false;
+        }
     }
 }
