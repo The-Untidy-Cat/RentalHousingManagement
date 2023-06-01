16374e035f25d6e790bfaaad9431892d5627d606
Whitie017 <nguyenngoc091203@gmail.com>
2023-06-01 22:38:56 +0700

    update ContractManagemet and ContractUpdateDialog

--- src/main/java/com/theuntidycat/rhm/view/ContractUpdateDialog.java
+++ src/main/java/com/theuntidycat/rhm/view/ContractUpdateDialog.java
@@ -125,37 +125,13 @@ public class ContractUpdateDialog extends javax.swing.JFrame {
 
     /**
      * @param args the command line arguments
+     * @return 
+     * 
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ContractUpdateDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ContractUpdateDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ContractUpdateDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ContractUpdateDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ContractUpdateDialog().setVisible(true);
-            }
-        });
+    
+    public javax.swing.JTextField getTxtRoomID()
+    {
+        return txtRoomID;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
