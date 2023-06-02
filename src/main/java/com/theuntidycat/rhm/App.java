5435a489ab43eedb99352a9fad5f3f4cc0704178
sheroanh <anhnguyentuan8@gmail.com>
2023-06-02 14:54:33 +0700

    fix bugs & big update

--- src/main/java/com/theuntidycat/rhm/App.java
+++ src/main/java/com/theuntidycat/rhm/App.java
@@ -1,20 +1,18 @@
 package com.theuntidycat.rhm;
 
-import com.theuntidycat.rhm.view.LoginView;
+import com.theuntidycat.rhm.view.login.LoginView;
 
-import com.theuntidycat.rhm.view.MainView;
+import com.theuntidycat.rhm.view.main.MainView;
 
 import com.formdev.flatlaf.FlatLightLaf;
 
 public class App {
+
     private static LoginView loginWindow = new LoginView();
     private static MainView mainWindow = new MainView();
-    
+
     public static void main(String[] args) {
-        FlatLightLaf.setup(); 
+        FlatLightLaf.setup();
         loginWindow.run(mainWindow);
-        
     }
 }
-
-
